3ec359850ba316602ef572327b755938448eb383
rafaelsantos <rafaelalonsobarbosa@gmail.com>
2021-09-02 20:19:35 -0300

    decimo requisito finalizado

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,16 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+/** Crie uma função que recebe um array de nomes de tecnologias que você quer aprender. Essa função deve receber também um segundo parâmetro chamado 'name' com um nome. */
+function techList(array, names) {
+  array = array.sort();
+  let tecnologies = [];
+  if (array.length === 0) {
+    return 'Vazio!';
+  }
+  for (let index = 0; index < array.length; index += 1) {
+    let tecnologi = { tech: array[index], name: names };
+    tecnologies.push(tecnologi);
+  }
+  return tecnologies;
 }
 
 // Desafio 11
