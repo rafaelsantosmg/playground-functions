a10ea3578c0521dd3114279c05595271d8d87645
rafaelsantos <rafaelalonsobarbosa@gmail.com>
2021-09-03 00:36:55 -0300

    decimo primeiro requisito finalizado

--- src/challenges2.js
+++ src/challenges2.js
@@ -14,8 +14,75 @@ function techList(array, names) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+/** Crie uma função chamada generatePhoneNumber que receba uma array com 11 números e retorne um número de telefone, respeitando parênteses, traços e espaços.
+
+Exemplo: caso o parâmetro da função seja [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1], generatePhoneNumber deverá retornar (12) 34567-8901. */
+function howManyRepeat(value, array) {
+  let totRepeat = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    if (value === array[index]) {
+      totRepeat += 1;
+    }
+    if (totRepeat >= 3) {
+      return totRepeat;
+    }
+  }
+  return totRepeat;
+}
+
+function checkNumberRepeat(array) {
+  let totRepeat = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    totRepeat = howManyRepeat(array[index], array);
+    if (totRepeat >= 3) {
+      return 'true';
+    }
+  }
+}
+
+function checkNumberInvalid(array) {
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] > 9 || array[index] < 0) {
+      return 'true';
+    }
+  }
+}
+
+function arrayByString(array) {
+  let string = '';
+  for (let index = 0; index < 13; index += 1) {
+    string += array[index];
+  }
+  return string;
+}
+
+function formatNumberPhone(array) {
+  let string = arrayByString(array);
+  let ddd = string.slice(0, 2);
+  let firstNumber = string.slice(2, 7);
+  let lastNumber = string.slice(7, 11);
+  let phoneNumber = [ddd, firstNumber, lastNumber];
+  return phoneNumber;
+}
+
+function displayNumberPhone(array) {
+  let phoneNumber = formatNumberPhone(array);
+  let phone = `(${phoneNumber[0]}) ${phoneNumber[1]}-${phoneNumber[2]}`;
+  return phone;
+}
+
+function generatePhoneNumber(array) {
+  let invalidNumber = checkNumberInvalid(array);
+  let invalidRepeat = checkNumberRepeat(array);
+  if (array.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  if (invalidNumber === 'true' || invalidRepeat === 'true') {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+  let phoneNumber = displayNumberPhone(array);
+
+  return phoneNumber;
 }
 
 // Desafio 12
